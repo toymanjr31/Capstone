f39df8565f7b6fcaba5701fffe59fd89f26765d2
toymanjr31 <toymanjr31@gmail.com>
2021-05-09 20:18:47 +0700

    Initial commit

---
+++ Android/app/src/main/java/com/dicoding/androidprogramming/capstone/MainActivity.kt
@@ -0,0 +1,89 @@
+package com.dicoding.androidprogramming.capstone
+
+import android.content.Intent
+import android.graphics.Bitmap
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.MediaStore
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import com.dicoding.androidprogramming.capstone.ml.ConvertedModel
+import org.tensorflow.lite.DataType
+import org.tensorflow.lite.support.image.TensorImage
+import org.tensorflow.lite.support.tensorbuffer.TensorBuffer
+
+class MainActivity : AppCompatActivity() {
+    lateinit var select_image_button : Button
+    lateinit var make_prediction : Button
+    lateinit var img_view : ImageView
+    lateinit var text_view : TextView
+    lateinit var bitmap: Bitmap
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        select_image_button = findViewById(R.id.button)
+        make_prediction = findViewById(R.id.button2)
+        img_view = findViewById(R.id.imageView2)
+        text_view = findViewById(R.id.textView)
+
+        val labels = application.assets.open("label.txt").bufferedReader().use { it.readText() }.split("\n")
+
+        select_image_button.setOnClickListener(View.OnClickListener {
+            var intent = Intent(Intent.ACTION_GET_CONTENT)
+            intent.type = "image/*"
+
+            startActivityForResult(intent, 100)
+        })
+
+        make_prediction.setOnClickListener(View.OnClickListener {
+            var resized = Bitmap.createScaledBitmap(bitmap, 224*4, 224, true)
+
+            val model = ConvertedModel.newInstance(this)
+
+            var tbuffer = TensorImage.fromBitmap(resized)
+            var byteBuffer = tbuffer.buffer
+
+            val inputFeature0 = TensorBuffer.createFixedSize(intArrayOf(1, 224, 224, 3), DataType.FLOAT32)
+            inputFeature0.loadBuffer(byteBuffer)
+
+
+            val outputs = model.process(inputFeature0)
+            val outputFeature0 = outputs.outputFeature0AsTensorBuffer
+
+            var max = getMax(outputFeature0.floatArray)
+
+            text_view.setText(labels[max])
+
+            model.close()
+        })
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        img_view.setImageURI(data?.data)
+
+        var uri: Uri? = data?.data
+
+        bitmap = MediaStore.Images.Media.getBitmap(this.contentResolver, uri)
+    }
+
+    fun getMax(arr:FloatArray) : Int{
+        var ind = 0;
+        var min = 0.0f;
+
+        for(i in 0..1)
+        {
+            if(arr[i] > min)
+            {
+                min = arr[i]
+                ind = i;
+            }
+        }
+        return ind
+    }
+}
