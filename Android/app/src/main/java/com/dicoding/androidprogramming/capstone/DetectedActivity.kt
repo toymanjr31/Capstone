ad0208c154d5f018d2f2ba84079e6fc4d9b70194
toymanjr31 <toymanjr31@gmail.com>
2021-05-17 21:12:39 +0700

    Add Confidence

--- Android/app/src/main/java/com/dicoding/androidprogramming/capstone/DetectedActivity.kt
+++ Android/app/src/main/java/com/dicoding/androidprogramming/capstone/DetectedActivity.kt
@@ -9,6 +9,7 @@ class DetectedActivity : AppCompatActivity() {
     companion object{
         const val EXTRA_INDEX ="extra_index"
         const val EXTRA_CLASS = "extra_class"
+        const val EXTRA_CONFIDENCE = "extra_confidence"
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,8 +19,10 @@ class DetectedActivity : AppCompatActivity() {
 
         val detectedClassIndex = intent.getIntExtra(EXTRA_INDEX, 0)
         val detectedClassName = intent.getStringExtra(EXTRA_CLASS)
+        val detectedConfidence = intent.getStringExtra(EXTRA_CONFIDENCE)
 
         activityDetectedBinding.tvDetectedClass.text = detectedClassName
+        activityDetectedBinding.tvConfidence.text = detectedConfidence
 
         if (detectedClassIndex == 0){
             activityDetectedBinding.tvDescriptionDetail.text = "This is description for cat"
--- Android/app/src/main/java/com/dicoding/androidprogramming/capstone/MainActivity.kt
+++ Android/app/src/main/java/com/dicoding/androidprogramming/capstone/MainActivity.kt
@@ -6,87 +6,84 @@ import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
-import android.view.View
-import android.widget.Button
 import android.widget.ImageView
-import android.widget.TextView
+import com.dicoding.androidprogramming.capstone.databinding.ActivityMainBinding
 import com.dicoding.androidprogramming.capstone.ml.ConvertedModel
 import org.tensorflow.lite.DataType
 import org.tensorflow.lite.support.image.TensorImage
 import org.tensorflow.lite.support.tensorbuffer.TensorBuffer
 
 class MainActivity : AppCompatActivity() {
-    lateinit var select_image_button : Button
-    lateinit var make_prediction : Button
-    lateinit var img_view : ImageView
-    lateinit var text_view : TextView
+    lateinit var imgView : ImageView
     lateinit var bitmap: Bitmap
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(activityMainBinding.root)
 
-        select_image_button = findViewById(R.id.button)
-        make_prediction = findViewById(R.id.button2)
-        img_view = findViewById(R.id.imageView2)
-        text_view = findViewById(R.id.textView)
+        imgView = activityMainBinding.imageView2
 
         val labels = application.assets.open("label.txt").bufferedReader().use { it.readText() }.split("\n")
 
-        select_image_button.setOnClickListener(View.OnClickListener {
-            var intent = Intent(Intent.ACTION_GET_CONTENT)
+        activityMainBinding.button.setOnClickListener {
+            val intent = Intent(Intent.ACTION_GET_CONTENT)
             intent.type = "image/*"
 
             startActivityForResult(intent, 100)
-        })
+        }
 
-        make_prediction.setOnClickListener(View.OnClickListener {
-            var resized = Bitmap.createScaledBitmap(bitmap, 224*4, 224, true)
+        activityMainBinding.button2.setOnClickListener {
+            val resized = Bitmap.createScaledBitmap(bitmap, 224 * 4, 224, true)
 
             val model = ConvertedModel.newInstance(this)
 
-            var tbuffer = TensorImage.fromBitmap(resized)
-            var byteBuffer = tbuffer.buffer
+            val tbuffer = TensorImage.fromBitmap(resized)
+            val byteBuffer = tbuffer.buffer
 
-            val inputFeature0 = TensorBuffer.createFixedSize(intArrayOf(1, 224, 224, 3), DataType.FLOAT32)
+            val inputFeature0 =
+                TensorBuffer.createFixedSize(intArrayOf(1, 224, 224, 3), DataType.FLOAT32)
             inputFeature0.loadBuffer(byteBuffer)
 
 
             val outputs = model.process(inputFeature0)
             val outputFeature0 = outputs.outputFeature0AsTensorBuffer
 
-            var max = getMax(outputFeature0.floatArray)
+            val max = getMax(outputFeature0.floatArray)
+
+            val confidence = outputFeature0.floatArray[max].toString()
 
             val intent = Intent(this@MainActivity, DetectedActivity::class.java)
             intent.putExtra(DetectedActivity.EXTRA_INDEX, max)
             intent.putExtra(DetectedActivity.EXTRA_CLASS, labels[max])
+            intent.putExtra(DetectedActivity.EXTRA_CONFIDENCE, confidence)
             startActivity(intent)
 
             //text_view.setText(labels[max])
 
             model.close()
-        })
+        }
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        img_view.setImageURI(data?.data)
+        imgView.setImageURI(data?.data)
 
-        var uri: Uri? = data?.data
+        val uri: Uri? = data?.data
 
         bitmap = MediaStore.Images.Media.getBitmap(this.contentResolver, uri)
     }
 
     fun getMax(arr:FloatArray) : Int{
-        var ind = 0;
-        var min = 0.0f;
+        var ind = 0
+        var min = 0.0f
 
         for(i in 0..1)
         {
             if(arr[i] > min)
             {
                 min = arr[i]
-                ind = i;
+                ind = i
             }
         }
         return ind
